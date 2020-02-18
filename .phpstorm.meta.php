531d2f8805d38c04a927488b1b580559e10cc4fd
MaxLovesWeb <4336944@gmail.com>
2020-02-18 16:45:13 +0100

    added Tucker-Eric/EloquentFilter package for filter Eloquent Models and their relationships

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-17 20:09:30.
+    * Generated on 2020-02-18 15:44:04.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
